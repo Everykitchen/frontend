c6bc0545856012cef3806045bf05da7d679628ad
MinjeMun <mmj010220@gmail.com>
2025-04-14 16:26:51 +0900

    rename: reservation 경로 수정

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import MyPage from "../pages/MyPage";
-import { Reservations } from "../pages/mypage/index";
+import { Reservations } from "../pages/mypage/MyPageIndex";
 
 const AppRouter = () => {
     return (
