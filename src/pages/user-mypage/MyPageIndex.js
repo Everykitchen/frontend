c248aaf62623f07b9f8697ba1f37f6aa9f1813cb
MinjeMun <mmj010220@gmail.com>
2025-04-24 05:38:39 +0900

    fix: MyPage 호출 위치 수정

--- src/pages/user-mypage/MyPageIndex.js
+++ src/pages/user-mypage/MyPageIndex.js
@@ -1,3 +1,4 @@
+export { default as MyPage } from "./MyPage";
 export { default as Reservations } from "./Reservations";
 export { default as ReservationDetail } from "./ReservationDetail";
 export { default as Reviews } from "./Reviews";
